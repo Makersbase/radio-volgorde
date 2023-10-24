9da4318071274bd0785f94e00af3df5b0d6a890a
Makersbase <148645971+Makersbase@users.noreply.github.com>
2023-10-24 17:05:27 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,29 @@
 function ChannelSearch () {
     while (Found_Channel != Search_Channel) {
-        Found_Channel = Search_Channel
-        radio.sendValue("Send_Channel", Search_Channel)
-        basic.pause(100)
+        radio.setGroup(Search_Channel)
+        radio.sendValue("SChannel", Search_Channel)
+        basic.pause(1000)
+        if (EchoTerug == 1) {
+            EchoTerug = 0
+        } else {
+            Found_Channel = Search_Channel
+        }
     }
-    basic.showNumber(Found_Channel)
+    basic.showNumber(Search_Channel)
 }
 radio.onReceivedValue(function (name, value) {
-    if (name == "Send_Channel") {
-        if (value == Found_Channel) {
-            radio.sendValue("Channel_occupied", 1)
-        }
+    if (name == "SChannel") {
+        radio.sendValue("OChannel", 1)
     }
-    if (name == "Channel_occupied") {
-        if (value == 1) {
-            Search_Channel += 1
-            basic.showNumber(Search_Channel)
-        }
+    if (name == "OChannel") {
+        Search_Channel += 1
+        EchoTerug += 1
     }
 })
+let EchoTerug = 0
 let Search_Channel = 0
 let Found_Channel = 0
 Found_Channel = 0
 Search_Channel = 1
-radio.setGroup(1)
+EchoTerug = 0
 ChannelSearch()
