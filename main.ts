8e861d90308ae8f100db65f5aa37529067e5f793
Makersbase <148645971+Makersbase@users.noreply.github.com>
2023-10-25 16:38:38 +0200

    Function Cleaner

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 function ChannelSearch () {
+    Found_Channel = 0
+    Search_Channel = 1
+    EchoTerug = 0
     while (Found_Channel != Search_Channel) {
         radio.setGroup(Search_Channel)
         radio.sendValue("SChannel", Search_Channel)
@@ -9,7 +12,6 @@ function ChannelSearch () {
             Found_Channel = Search_Channel
         }
     }
-    basic.showNumber(Search_Channel)
 }
 radio.onReceivedValue(function (name, value) {
     if (name == "SChannel") {
@@ -21,9 +23,7 @@ radio.onReceivedValue(function (name, value) {
     }
 })
 let EchoTerug = 0
-let Search_Channel = 0
 let Found_Channel = 0
-Found_Channel = 0
-Search_Channel = 1
-EchoTerug = 0
+let Search_Channel = 0
 ChannelSearch()
+basic.showNumber(Search_Channel)
